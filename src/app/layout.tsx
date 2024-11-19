fc2c48e7627a28aa715ebcf1c0ffce52c3b96818
An-On-Y-mous <kishorerajendran2003@gmail.com>
2024-11-19 23:24:18 +0530

    Added Custom fonts to layout.tsx page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,41 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import {
+  Inter,
+  Poppins,
+  Nunito,
+  Josefin_Sans,
+  Bebas_Neue,
+} from "next/font/google";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 
+const inter = Inter({ subsets: ["latin"] });
+
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "400", "500", "600", "800"],
+  variable: "--font-poppins",
+});
+const nunito = Nunito({
+  subsets: ["latin"],
+  weight: ["200", "400", "500", "600", "800"],
+  variable: "--font-nunito",
+});
+const josefin = Josefin_Sans({
+  subsets: ["latin"],
+  weight: ["200", "400", "500", "600", "700"],
+  variable: "--font-josefin",
+});
+const bebas = Bebas_Neue({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-bebas",
+});
+
 export const metadata: Metadata = {
   title: "kramola",
-  description: "A Multilanguage News Website",
+  description: "A Multilingual News Website",
 };
 
 export default function RootLayout({
@@ -15,7 +45,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
+      <body
+        className={`${inter.className} ${poppins.variable} ${nunito.variable} ${josefin.variable} ${bebas.variable}`}
+      >
         <Header />
         {children}
         <Footer />
