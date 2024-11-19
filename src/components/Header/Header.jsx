677448268cb2cb813822fb328ef17053436d1639
An-On-Y-mous <kishorerajendran2003@gmail.com>
2024-11-19 23:40:10 +0530

    "Updated Header component with new structure and styles"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,18 @@
+import Link from "next/link";
 import "./Header.scss";
 const Header = ({}) => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="nav-parent">
+      <div className="nav-container">
+        <div className="nav-title">
+          <h1>kramola</h1>
+        </div>
+        <ul>
+          <Link href={"/"}>Home</Link>
+          <li>Newsletter</li>
+          <Link href={"/aboutus"}>About Us</Link>
+        </ul>
+      </div>
     </div>
   );
 };
