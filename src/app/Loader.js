12b1a5c8a7822b3a4bd4dff999291482e29290db
An-On-Y-mous <kishorerajendran2003@gmail.com>
2024-11-23 01:00:45 +0530

    "Added @mantine/hooks dependency, removed Loader.jsx component, and updated yarn.lock"

---
+++ src/app/Loader.js
@@ -0,0 +1,7 @@
+import "../styles/loading.scss";
+
+const Loader = () => {
+  return <div className="loader"></div>;
+};
+
+export default Loader;
--- src/components/ui/Loader.jsx
+++
@@ -1,5 +0,0 @@
-import { Loader } from "@mantine/core";
-
-function Demo() {
-  return <Loader color="gray" />;
-}
