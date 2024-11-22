34b9919a9acdc438f1fbdc336947ca77f3a699d8
An-On-Y-mous <kishorerajendran2003@gmail.com>
2024-11-22 19:46:23 +0530

    "Updated image domains in next.config.ts"

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ["image-cdn.example.com", "globalnews.ca", "i2.wp.com"],
+    domains: [
+      "image-cdn.example.com",
+      "globalnews.ca",
+      "i2.wp.com",
+      "img-s-msn-com.akamaized.net",
+      "",
+    ],
   },
 };
 
