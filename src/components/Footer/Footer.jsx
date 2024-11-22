f74fb13c37d1620634c76fcb8f5cf647bc9fd8d4
An-On-Y-mous <kishorerajendran2003@gmail.com>
2024-11-23 00:27:51 +0530

    Updated the Footer component

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,28 @@
+import Link from "next/link";
 import "./Footer.scss";
+
 const Footer = ({}) => {
   return (
-    <div>
-      <h1>Footer</h1>
+    <div className="footer-parent">
+      <div className="footer-container">
+        <div className="footer-left">
+          <h3>kramola</h3>
+          <p>Breaking Political News Across the Globe in Multi-Language</p>
+        </div>
+        <div className="footer-right">
+          <ul>
+            <li>
+              <Link href="#">Privacy Policy</Link>
+            </li>
+            <li>
+              <Link href="#">Terms of Service</Link>
+            </li>
+            <li>
+              <Link href="/aboutus">Contact Us</Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
