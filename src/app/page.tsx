c63a83b4938d0550bdc93f36ba747db5dac67e7c
An-On-Y-mous <kishorerajendran2003@gmail.com>
2024-11-23 01:39:42 +0530

    "Updated globals.css to change background and text colors, and refactored page.tsx to add loader, restructure layout, and modify rendering of news components."

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,36 @@
+"use client";
 import RenderNews from "@/components/renderNews/renderNews";
+import { useState, useEffect } from "react";
 import "@/styles/styles.scss";
+import Loader from "@/app/Loader";
 export default function Home() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setLoading(false);
+  });
   return (
-    <div className="parent">
-      <div className="parent-container">
-        <div className="left">
-          <RenderNews limit={1} />
+    <div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="parent">
+          <div className="parent-container">
+            <div className="left">
+              <RenderNews limit={1} />
+            </div>
+            <div className="right">
+              <RenderNews limit={4} startIndex={1} />
+            </div>
+          </div>
+          <h1 className="font-bebas text-[48px] text-center my-[3vh] mx-auto">
+            Latest News
+          </h1>
+          <div className="grid-view">
+            <RenderNews startIndex={5} />
+          </div>
         </div>
-        <div className="right">
-          <RenderNews limit={4} startIndex={1} />
-        </div>
-      </div>
-      <h1 className="font-bebas text-[48px] text-center my-[3vh] mx-auto">
-        Latest News
-      </h1>
-      <div className="grid-view">
-        <RenderNews startIndex={5} />
-      </div>
+      )}
     </div>
   );
 }
