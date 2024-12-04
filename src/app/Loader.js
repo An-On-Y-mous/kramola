66897e42149e7302b7c08d6faad7e53345f55576
An-On-Y-mous <kishorerajendran2003@gmail.com>
2024-12-04 22:08:35 +0530

    "Change the loading styles and adjust the position"

--- src/app/Loader.js
+++ src/app/Loader.js
@@ -1,7 +1,11 @@
 import "../styles/loading.scss";
 
 const Loader = () => {
-  return <div className="loader"></div>;
+  return (
+    <div className="parent">
+      <div className="loader"></div>
+    </div>
+  );
 };
 
 export default Loader;
