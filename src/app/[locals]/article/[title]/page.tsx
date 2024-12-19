cb42d1a30aa273e902a75181291e29db715ccc47
An-On-Y-mous <kishorerajendran2003@gmail.com>
2024-12-19 19:50:05 +0530

    Create a dynamic route for the article page

---
+++ src/app/[locals]/article/[title]/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+interface Props {}
+
+const ArticleLocale: NextPage<Props> = ({}) => {
+  return (
+    <div>
+      <h1>Other language Articles</h1>
+    </div>
+  );
+};
+
+export default ArticleLocale;
---
+++ src/app/article/[title]/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from "next";
+
+interface Props {}
+
+const Article: NextPage<Props> = ({}) => {
+  return (
+    <div>
+      <h1>English language article</h1>
+    </div>
+  );
+};
+
+export default Article;
