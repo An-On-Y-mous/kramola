c7771ed731b0fbe5d7f2c2e1e7b1688a59de4fae
An-On-Y-mous <kishorerajendran2003@gmail.com>
2024-12-19 19:49:20 +0530

    `Refactor Home function and styles.scss to remove commented out code and unused variables & try to change the img height and width`

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,16 +3,11 @@ import "@/styles/styles.scss";
 
 export default async function Home() {
   const fetchNews = async () => {
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/fetchNews?locale=en`
-      ); // Adjust the locale as needed
-      const data = await response.json();
-      return data;
-    } catch (error) {
-      console.error("Error fetching news:", error);
-    } finally {
-    }
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/fetchNews?locale=en`
+    );
+    const data = await response.json();
+    return data;
   };
   const news = await fetchNews();
 
@@ -21,17 +16,17 @@ export default async function Home() {
       <div className="parent">
         <div className="parent-container">
           <div className="left">
-            <RenderNews newsLocale={news} limit={1} />{" "}
+            <RenderNews newsLocale={news} limit={1} />
           </div>
           <div className="right">
-            <RenderNews newsLocale={news} limit={5} startIndex={1} />{" "}
+            <RenderNews newsLocale={news} limit={5} startIndex={1} />
           </div>
         </div>
         <h1 className="font-bebas text-[48px] text-center my-[3vh] mx-auto">
           Latest News
         </h1>
         <div className="grid-view">
-          <RenderNews newsLocale={news} startIndex={6} />{" "}
+          <RenderNews newsLocale={news} startIndex={6} />
         </div>
       </div>
     </div>
