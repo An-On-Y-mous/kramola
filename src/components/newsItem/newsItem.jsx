35bac7903ff81f9afc1171823177efd1e9ab9778
An-On-Y-mous <kishorerajendran2003@gmail.com>
2025-01-15 02:55:28 +0530

    Implement a link for overall div

--- src/components/newsItem/newsItem.jsx
+++ src/components/newsItem/newsItem.jsx
@@ -15,88 +15,49 @@ const NewsItem = ({
 }) => {
   return (
     <div className="news-item">
-      <div className="news-container">
-        <div className="news-image">
-          <Link
-            href={
-              !locale || locale === "en"
-                ? `/article/${slugTitle}`
-                : `/${locale}/article/${slugTitle}`
-            }
-          >
-            <Image
-              src={img_url || "/default-fallback-image.png"}
-              width={1200}
-              height={600}
-              alt="image"
-            />
-          </Link>
-        </div>
-        <div className="news-subcontainer">
-          <div className="news-title">
-            <Link
-              href={
-                !locale || locale === "en"
-                  ? `/article/${slugTitle}`
-                  : `/${locale}/article/${slugTitle}`
-              }
-            >
-              <h3>{title}</h3>
-            </Link>
-          </div>
-          <div>
-            <Link
-              href={
-                !locale || locale === "en"
-                  ? `/article/${slugTitle}`
-                  : `/${locale}/article/${slugTitle}`
-              }
-            >
-              <div
-                className="news-description"
-                dangerouslySetInnerHTML={{ __html: description }}
-              ></div>
-            </Link>
-          </div>
+      <Link
+        href={
+          !locale || locale === "en"
+            ? `/article/${slugTitle}`
+            : `/${locale}/article/${slugTitle}`
+        }
+        className="news-container"
+      >
+        <span className="news-image">
+          <Image
+            src={img_url || "/default-fallback-image.png"}
+            width={1200}
+            height={600}
+            alt="image"
+          />
+        </span>
+        <span className="news-subcontainer">
+          <span className="news-title">
+            <h3>{title}</h3>
+          </span>
+          <span>
+            <span
+              className="news-description"
+              dangerouslySetInnerHTML={{ __html: description }}
+            ></span>
+          </span>
 
-          <div className="date-read">
-            <div className="news-read-more-link">
-              <Link
-                href={
-                  intReadMore
-                    ? locale
-                      ? `/${locale}/article/${slugTitle}`
-                      : `/article/${slugTitle}`
-                    : source_url
-                }
-                target={intReadMore ? "" : "_blank"}
-                rel="noopener noreferrer"
-              >
-                Read More
-              </Link>
-            </div>
-            <div className="news-date">
-              <Link
-                href={
-                  !locale || locale === "en"
-                    ? `/article/${slugTitle}`
-                    : `/${locale}/article/${slugTitle}`
-                }
-              >
-                <p>
-                  {date
-                    ? new Date(date).toLocaleDateString("en-US", {
-                        year: "numeric",
-                        month: "long",
-                        day: "numeric",
-                      })
-                    : "Loading..."}
-                </p>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </div>
+          <span className="date-read">
+            <span className="news-read-more-link">Read More</span>
+            <span className="news-date">
+              <p>
+                {date
+                  ? new Date(date).toLocaleDateString("en-US", {
+                      year: "numeric",
+                      month: "long",
+                      day: "numeric",
+                    })
+                  : "Loading..."}
+              </p>
+            </span>
+          </span>
+        </span>
+      </Link>
     </div>
   );
 };
