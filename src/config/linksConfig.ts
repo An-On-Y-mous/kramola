88e6e30a81a3ea2f09e5f278238f9e97e473855f
An-On-Y-mous <kishorerajendran2003@gmail.com>
2025-01-16 22:00:17 +0530

    `Update internal and external links configuration with new entries and translations`

--- src/config/linksConfig.ts
+++ src/config/linksConfig.ts
@@ -19,15 +19,33 @@ Example Format:
 export const internalLinks = [
   {
     word: "Biden",
+    link: "/article/musk-ramaswamy-put-spotlight-on-proliferation-of-us-regulations",
+    translations: { ru: "Байдена", es: "Biden" },
+  },
+  {
+    word: "Trump",
     link: "/article/how-to-not-lose-it-completely-during-trumps-second-presidency",
-    translations: { ru: "Байден", es: "Biden" },
+    translations: { ru: "Трамп", es: "Trump" },
   },
 ];
 
 export const externalLinks = [
   {
-    word: "rulemakings",
-    link: "https://example-site.com",
-    translations: { ru: "регламенты", es: "reglamentos" },
+    word: "Congressional Review Act",
+    link: "https://en.wikipedia.org/wiki/Congressional_Review_Act",
+    translations: {
+      ru: "119",
+      es: "Acto de Revisión del Congreso",
+    },
+  },
+  {
+    word: "Elon Musk",
+    link: "https://en.wikipedia.org/wiki/Elon_Musk",
+    translations: { ru: "Илон Маск", es: "Elon Musk" },
+  },
+  {
+    word: "Biden",
+    link: "https://en.wikipedia.org/wiki/Joe_Biden",
+    translations: { ru: "Байдена", es: "Biden" },
   },
 ];
